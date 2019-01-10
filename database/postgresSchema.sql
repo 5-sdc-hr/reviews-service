913a7b60e6e94e0a8f7241a7e357051ff29e5f10
Ron Arbel <ronarbel1@gmail.com>
2019-01-10 02:10:58 -0800

    seed two sampledata lines into db

--- database/postgresSchema.sql
+++ database/postgresSchema.sql
@@ -6,27 +6,6 @@ CREATE DATABASE sdc;
 
 CREATE SCHEMA reserveme;
 
--- CREATE TABLE reserveme.reviews(
---   id SERIAL NOT NULL,
---   restaurant_id INTEGER,
---   reviewer_id INTEGER,
---   reviewer_nickname VARCHAR(20),
---   CONSTRAINT persons_pkey PRIMARY KEY (id)
--- );
-
--- INSERT INTO reserveme.reviews(
---   restaurant_id,
---   reviewer_id,
---   reviewer_nickname
--- )
--- VALUES (
---   1,
---   3001,
---   'Ramentic'
--- );
-
-
-
 CREATE TABLE reserveme.reviews(
   id SERIAL NOT NULL,
   restaurant_id INTEGER,
@@ -49,8 +28,7 @@ CREATE TABLE reserveme.reviews(
   CONSTRAINT persons_pkey PRIMARY KEY (id)
 );
 
-INSERT INTO reserveme.reviews
-(
+COPY reserveme.reviews(
   restaurant_id,
   reviewer_id,
   reviewer_nickname,
@@ -69,27 +47,35 @@ INSERT INTO reserveme.reviews
   review_helpful_count,
   review_tags
 )
-VALUES (
-  1,
-  3001,
-  'Ramentic',
-  'San Francisco',
-  1,
-  '2018-12-09T06:33:48.261Z',
-  3001,
-  5,
-  4,
-  4,
-  3,
-  4,
-  'moderate',
-  true,
-  'It was just ok nothing special. Steak was not seasoned well. Hen of the mushrooms was greasy. Mentioned it was my guest birthday that night- nada. Very noisy.',
-  0,
-  '{"Special Occasion"}'
-);
+FROM '/Users/ronarbel/Desktop/reviews-service/database/generatedData.csv' CSV DELIMITER ',';
 
--- COPY reserveme.reviews(
+
+
+
+-- CREATE TABLE reserveme.reviews(
+--   id SERIAL NOT NULL,
+--   restaurant_id INTEGER,
+--   reviewer_id INTEGER,
+--   reviewer_nickname VARCHAR(20),
+--   reviewer_location VARCHAR(20),
+--   reviewer_review_count SMALLINT,
+--   reviewer_date_dined TIMESTAMP,
+--   review_id INTEGER,
+--   review_ratings_overall SMALLINT,
+--   review_ratings_food SMALLINT,
+--   review_ratings_service SMALLINT,
+--   review_ratings_ambience SMALLINT,
+--   review_ratings_value SMALLINT,
+--   review_ratings_noise_level VARCHAR(15),
+--   review_recommend_to_friend BOOLEAN,
+--   review_text TEXT,
+--   review_helpful_count SMALLINT,
+--   review_tags TEXT [],
+--   CONSTRAINT persons_pkey PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO reserveme.reviews
+-- (
 --   restaurant_id,
 --   reviewer_id,
 --   reviewer_nickname,
@@ -106,9 +92,49 @@ VALUES (
 --   review_recommend_to_friend,
 --   review_text,
 --   review_helpful_count,
---   review_tags,
+--   review_tags
+-- )
+-- VALUES (
+--   1,
+--   3001,
+--   'Ramentic',
+--   'San Francisco',
+--   1,
+--   '2018-12-09T06:33:48.261Z',
+--   3001,
+--   5,
+--   4,
+--   4,
+--   3,
+--   4,
+--   'moderate',
+--   true,
+--   'It was just ok nothing special. Steak was not seasoned well. Hen of the mushrooms was greasy. Mentioned it was my guest birthday that night- nada. Very noisy.',
+--   0,
+--   '{"Special Occasion"}'
+-- );
+
+-- COPY reserveme.reviews
+-- (
+--   restaurant_id,
+--   reviewer_id,
+--   reviewer_nickname,
+--   reviewer_location,
+--   reviewer_review_count,
+--   reviewer_date_dined,
+--   review_id,
+--   review_ratings_overall,
+--   review_ratings_food,
+--   review_ratings_service,
+--   review_ratings_ambience,
+--   review_ratings_value,
+--   review_ratings_noise_level,
+--   review_recommend_to_friend,
+--   review_text,
+--   review_helpful_count,
+--   review_tags
 -- ) 
--- FROM '/Users/ronarbel/Desktop/reviews-service/database/generatedData.csv' DELIMITER ',';
+-- FROM '/Users/ronarbel/Desktop/reviews-service/database/generatedData.csv' CSV DELIMITER ',';
 
 
 /*  Execute this file from the command line by typing:
