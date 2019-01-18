8a90339ada34aff7911622a41775e0fa4e53cabf
Ron Arbel <ronarbel1@gmail.com>
2019-01-17 17:03:00 -0800

    query postgres instead of mongo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-const port = 3004;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const dbMongo = require('../database/mongoIndex.js');
+const dbPostgres = require('../database/postgresIndex.js');
 
 app.use(morgan('tiny'));
 app.use(bodyParser());
@@ -19,18 +19,31 @@ app.use((req, res, next) => {
   next();
 });
 
+
+// // -------- mongo crud -------- //
+// app.get('/api/restaurants/:id/reviews', (req, res) => {
+//   let sortQuery = 'newest';
+//   if (req.query.sort) {
+//     sortQuery = req.query.sort;
+//   }
+//   const parsedId = parseInt(req.params.id, 10);
+//   dbMongo.retrieveReviews(parsedId, sortQuery, (err, results) => {
+//     if (err) {
+//       res.status(404).end();
+//     }
+//     res.send(results);
+//   });
+// });
+
+// -------- postgres crud -------- //
 app.get('/api/restaurants/:id/reviews', (req, res) => {
-  let sortQuery = 'newest';
-  if (req.query.sort) {
-    sortQuery = req.query.sort;
-  }
+  console.log('HERE')
   const parsedId = parseInt(req.params.id, 10);
-  dbMongo.retrieveReviews(parsedId, sortQuery, (err, results) => {
-    if (err) {
-      res.status(404).end();
-    }
-    res.send(results);
+  dbPostgres.retrieveReviews(parsedId, (reviews) => {
+    res.send(reviews);
   });
 });
 
+// -------- server -------- //
+const port = 3004;
 app.listen(port, () => console.log(`listening on port ${port}!`));
